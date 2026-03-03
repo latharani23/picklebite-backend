fd6a0939353745fdb5198993c7997ee797d3701a
Geetha MK <Geetha.mk562@gmail.com>
2026-03-03 13:53:12 +0530

    email send

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -159,10 +159,15 @@ router.put("/update-status/:id", protect, async (req, res) => {
       console.log("EMAIL_USER:", process.env.EMAIL_USER);
       console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
       const transporter = nodemailer.createTransport({
-        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // important
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS,
+        },
+        tls: {
+          rejectUnauthorized: false,
         },
       });
 
