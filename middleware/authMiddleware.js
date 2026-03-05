3e92d161a07d95b59c7b53a2674fc430f01c9b76
Latharani <latharani2305@gmail.com>
2026-03-05 20:19:19 +0530

    added userid

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,3 +1,46 @@
+// const jwt = require("jsonwebtoken");
+// const User = require("../models/User");
+
+// const protect = async (req, res, next) => {
+//   try {
+//     const authHeader = req.headers.authorization;
+
+//     if (!authHeader || !authHeader.startsWith("Bearer")) {
+//       return res.status(401).json({ message: "No token provided" });
+//     }
+
+//     const token = authHeader.split(" ")[1];
+
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+//     // ✅ ADMIN LOGIN
+//     if (decoded.role === "admin") {
+//       req.user = { role: "admin", email: decoded.email };
+//       return next();
+//     }
+
+//     // ✅ NORMAL USER
+//     const user = await User.findById(decoded.id).select("-password");
+
+//     if (!user) {
+//       return res.status(401).json({ message: "User not found" });
+//     }
+
+//     req.user = {
+//       id: user._id,
+//       username: user.username,
+//       email: user.email,
+//       role: "user",
+//     };
+
+//     next();
+//   } catch (error) {
+//     return res.status(401).json({ message: "Invalid token" });
+//   }
+// };
+
+// module.exports = protect;
+
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
@@ -13,13 +56,13 @@ const protect = async (req, res, next) => {
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // ✅ ADMIN LOGIN
+    // ADMIN LOGIN
     if (decoded.role === "admin") {
       req.user = { role: "admin", email: decoded.email };
       return next();
     }
 
-    // ✅ NORMAL USER
+    // NORMAL USER
     const user = await User.findById(decoded.id).select("-password");
 
     if (!user) {
@@ -27,7 +70,7 @@ const protect = async (req, res, next) => {
     }
 
     req.user = {
-      id: user._id,
+      _id: user._id, // ✅ FIX HERE
       username: user.username,
       email: user.email,
       role: "user",
