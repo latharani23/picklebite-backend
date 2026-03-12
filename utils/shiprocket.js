606b42d1fe65c79980138aed4f2252fa8642c956
Latharani <latharani2305@gmail.com>
2026-03-13 01:19:55 +0530

    added shiprocket

--- utils/shiprocket.js
+++ utils/shiprocket.js
@@ -61,44 +61,66 @@
 
 // module.exports = { createShipment };
 
-const response = await axios.post(
-  "https://apiv2.shiprocket.in/v1/external/orders/create/adhoc",
-  {
-    order_id: order._id.toString(),
-    order_date: new Date(),
+const axios = require("axios");
 
-    pickup_location: "Primary",
-
-    billing_customer_name: order.customer.name,
-    billing_last_name: ".", // REQUIRED FIELD
-
-    billing_address: order.customer.address,
-    billing_city: order.customer.city,
-    billing_pincode: order.customer.pincode,
-    billing_state: order.customer.state,
-    billing_country: "India",
-    billing_phone: order.customer.phone,
-
-    shipping_is_billing: true, // REQUIRED FIELD
-
-    order_items: order.items.map((item) => ({
-      name: item.name,
-      sku: item.name,
-      units: item.quantity,
-      selling_price: item.price,
-    })),
-
-    payment_method: "Prepaid",
-    sub_total: order.totalAmount,
-
-    length: 10,
-    breadth: 10,
-    height: 10,
-    weight: 0.5,
-  },
-  {
-    headers: {
-      Authorization: `Bearer ${token}`,
+async function getShiprocketToken() {
+  const login = await axios.post(
+    "https://apiv2.shiprocket.in/v1/external/auth/login",
+    {
+      email: process.env.SHIPROCKET_EMAIL,
+      password: process.env.SHIPROCKET_PASSWORD,
     },
-  },
-);
+  );
+
+  return login.data.token;
+}
+
+async function createShipment(order) {
+  const token = await getShiprocketToken();
+
+  const response = await axios.post(
+    "https://apiv2.shiprocket.in/v1/external/orders/create/adhoc",
+    {
+      order_id: order._id.toString(),
+      order_date: new Date(),
+
+      pickup_location: "Primary",
+
+      billing_customer_name: order.customer.name,
+      billing_last_name: ".",
+
+      billing_address: order.customer.address,
+      billing_city: order.customer.city,
+      billing_pincode: order.customer.pincode,
+      billing_state: order.customer.state,
+      billing_country: "India",
+      billing_phone: order.customer.phone,
+
+      shipping_is_billing: true,
+
+      order_items: order.items.map((item) => ({
+        name: item.name,
+        sku: item.name,
+        units: item.quantity,
+        selling_price: item.price,
+      })),
+
+      payment_method: "Prepaid",
+      sub_total: order.totalAmount,
+
+      length: 10,
+      breadth: 10,
+      height: 10,
+      weight: 0.5,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+
+  return response.data;
+}
+
+module.exports = { createShipment };
